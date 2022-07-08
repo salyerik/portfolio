8cff8834354f618c77566c84655a89137758d531
Stet <77533577+StetErik@users.noreply.github.com>
2022-07-08 21:21:30 +0600

    Add files via upload

---
+++ README.md
@@ -0,0 +1,16 @@
+# This is my portfolio site
+[Link to my portfolio⬅️][link]
+## I used here:
+* HTML (BEM, PUG)
+* CSS (SASS)
+* Pure JavaScript:
+	* Animations
+	* Menu-burger
+	* Tabs
+* GULP
+* NPM
+* GIT
+
+[Link to my portfolio⬅️][link]
+
+[link]: https://steterik.github.io/portfolio/
---
+++ files/mail.php
@@ -0,0 +1,18 @@
+<?php
+
+	$user_name = htmlspecialchars(urldecode(trim($_POST['user_name'])));
+	$user_mail = htmlspecialchars(urldecode(trim($_POST['user_mail'])));
+	$subject = htmlspecialchars(urldecode(trim($_POST['subject'])));
+	$user_message = htmlspecialchars(urldecode(trim($_POST['user_message'])));
+
+	if(mail("steterik@gmail.com",
+					"New letter from Portfolio",
+					"Name: ".$user_name."\n".
+					"Mail: ".$user_mail."\n".
+					"Subject: ".$subject."\n".
+					"Message: ".$user_message,
+					"From: portfolio \r\n")
+	){
+		header('Location: ../index.html');
+		exit;
+	}
